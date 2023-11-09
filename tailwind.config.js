fdca3c8a4ac7c5bf1cfe44258a7f310e8eb4f5b2
boomtnt2843 <boomphat@odds.team>
2023-11-10 00:37:15 +0700

    template & display section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      themeDarkBlue: '#161853',
+      themeLightBlue: '#292C6D',
+      themeWhite: '#FAEDF0',
+      themeRed: '#EC255A'
+    },
     extend: {},
   },
   plugins: [],
